1652beba556937ec3eff73c2b8a7311c89c357f9
Aryan <jumdearyan7@gmail.com>
2026-04-23 10:26:44 +0530

    Refactor Flask app with environment-based port
    Updated API response messages and added dynamic port configuration.

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Hello, Railway is working!"
+    return "🚀 Hello Railway!"
 
 @app.route("/api")
 def api():
-    return {"message": "This is your first deployed API"}
+    return {"message": "Working!"}
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port)
